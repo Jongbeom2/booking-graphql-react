dcb42bdc086a235ca3ad041ce195d69f49bd58be
DESKTOP-DH5BQM8\Lee <n11334@naver.com>
2020-07-12 16:53:23 +0900

    배포 설정 중

---
+++ client/build/precache-manifest.76d941390c221271ac8b28c6c34abc15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f122c5b669bdd11e9b938468035272bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "529ccafad469ab03ca16",
+    "url": "/static/css/main.3b0ca5af.chunk.css"
+  },
+  {
+    "revision": "2c68de9b35abc150bfed",
+    "url": "/static/js/2.804d1af9.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.804d1af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529ccafad469ab03ca16",
+    "url": "/static/js/main.ee4a4051.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8762902f929baca9f60e9bdb6cbce0e9",
+    "url": "/static/media/event.8762902f.png"
+  }
+]);
