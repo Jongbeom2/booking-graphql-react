627bed17ae3befdca2757df0ff1d85934f92fa5e
jongbeom <n113345@gmail.com>
2020-07-14 23:44:13 +0900

    Apollo Client event page에 개발

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+const client = new ApolloClient({
+  cache,
+  link
+});
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
--- client/src/pages/Event.js
+++ client/src/pages/Event.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -8,9 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import eventImg from '../img/event.png';
 import EventCreateDialog from '../components/EventCreateDialog';
-import axios from 'axios';
 import Event from '../components/Event';
 import LoadingContext from '../context/loadingContext';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 const useStyles = makeStyles(theme => ({
   createCard: {
     marginTop: theme.spacing(5),
@@ -24,46 +25,33 @@ const useStyles = makeStyles(theme => ({
 function EventPage() {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-  const [events, setEvents] = useState([]);
-  const {setIsLoading} = useContext(LoadingContext);
-  useEffect(() => {
-    getEvents();
-  }, []);
-  const getEvents = async () => {
-    const requestBody = {
-      query: `
-        query{
-          getEvents{
-            _id
-            title
-            description
-            price
-            date
-            creator{
-              _id
-              email
-            }
-          }
+  const { setIsLoading } = useContext(LoadingContext);
+  const GET_EVENTS = gql`
+    query eventLis{
+      getEvents{
+        _id
+        title
+        description
+        price
+        date
+        creator{
+          _id
+          email
         }
-      `
-    };
-    try {
-      setIsLoading(true);
-      const result = await axios({
-        url: '/graphql',
-        method: 'POST',
-        data: requestBody,
-      });
-      if (!result.data.data) {
-        throw new Error('Get events Failed');
       }
-      setEvents(result.data.data.getEvents);
-    } catch (err) {
-      alert('Get events Failed');
-      console.log(err);
-    } finally{
-      setIsLoading(false);
     }
+  `
+  const { data, loading, error } = useQuery(GET_EVENTS);
+  if (loading) {
+    setIsLoading(true);
+  }
+  if (data) {
+    setIsLoading(false);
+  }
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+  const getEvents = async () => {
+
   }
   const handleDialogOpen = () => {
     setOpen(true);
@@ -79,17 +67,16 @@ function EventPage() {
             <CardMedia
               className={classes.media}
               image={eventImg}
-              title="Contemplative Reptile"
             />
             <CardContent>
-              <Typography className={classes.title} variant ="h6" color="primary" gutterBottom>
+              <Typography className={classes.title} variant="h6" color="primary" gutterBottom>
                 Create event whatever you want!
               </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
-        {events.map(event =>
-          (<Event id = {event._id} title={event.title} description={event.description} price={event.price} date={event.date} creator={event.creator}/>)
+        {data?.getEvents.map(event =>
+          (<Event id={event._id} title={event.title} description={event.description} price={event.price} date={event.date} creator={event.creator} />)
         )}
       </Container>
       <EventCreateDialog handleClose={handleDialogClose} open={open} getEvents={getEvents} />
